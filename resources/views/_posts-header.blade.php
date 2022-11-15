83bc130ac7a3ee6199384b5dea9db4425795fc5d
Guilherme Diniz <guilhermedinizlau@gmail.com>
2022-11-15 20:49:46 -0300

    Finished Section 6 - Search

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -20,14 +20,14 @@
                     </button>
                 </x-slot>
 
-                <x-dropdown-item 
+                <x-dropdown-item
                     href="/"
                     :active="request()->routeIs('home')">
                         All
                 </x-dropdown-item>
 
                 @foreach ($categories as $category)
-                    <x-dropdown-item 
+                    <x-dropdown-item
                         href="/categories/{{ $category->slug }}"
                         :active="isset($currentCategory) && $currentCategory->is($category) ">
                             {{ ucwords($category->name) }}
@@ -39,8 +39,13 @@
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
-                        class="bg-transparent placeholder-black font-semibold text-sm">
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Find something"
+                    class="bg-transparent placeholder-black font-semibold text-sm"
+                    value="{{ request('search') }}"
+                >
             </form>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,20 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post,
-        'categories' => Category::all()
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
