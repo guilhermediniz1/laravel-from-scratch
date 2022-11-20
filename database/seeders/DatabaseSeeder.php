5550a6529e841fe1eecd4c7851aead31c7d041e4
Guilherme Diniz <guilhermedinizlau@gmail.com>
2022-11-20 17:18:14 -0300

    Finished Section 8 - Pagination

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Guilherme Diniz'
         ]);
 
-        Post::factory(5)->create([
+        Post::factory(30)->create([
             'user_id' => $user->id
         ]);
     }
